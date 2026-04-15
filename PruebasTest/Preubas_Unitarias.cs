f4a5425f91a9da0e6eac16345f25a6f6586beba1
vic <vinfocurso@gmail.com>
2026-04-15 23:41:45 +0200

    Creacion de Test y arreglo de errores 0.2.2

--- PruebasTest/Preubas_Unitarias.cs
+++ PruebasTest/Preubas_Unitarias.cs
@@ -1,19 +1,75 @@
-﻿using NUnit.Framework;
-using NUnit.Framework.Legacy;
-using Proyecto_Tareas;
+﻿using System;
+using NUnit.Framework;
+using Proyecto_Tareas.Dominio.Clases;
 
-namespace Proyecto_Tareas;
-
-public class Preubas_Unitarias
+namespace Proyecto_Tareas.PruebasTest
 {
-    [SetUp]
-    public void Setup()
+    public class Pruebas_Unitarias
     {
-    }
+        private Tarea tarea;
 
-    [Test]
-    public void Test1()
-    {
-        Assert.Pass();
+        [SetUp]
+        public void Setup()
+        {
+            tarea = new Tarea(
+                1,
+                "Tarea de prueba",
+                "Descripcion de prueba",
+                DateTime.Now.AddDays(2),
+                PrioridadTarea.Media
+            );
+        }
+
+        [Test]
+        public void CrearTareaComoPendiente()
+        {
+            Assert.That(tarea.Estado, Is.EqualTo(EstadoTarea.Pendiente));
+        }
+
+        [Test]
+        public void IniciarTareaEnProgreso()
+        {
+            tarea.Iniciar();
+
+            Assert.That(tarea.Estado, Is.EqualTo(EstadoTarea.EnProgreso));
+        }
+
+        [Test]
+        public void FinalizarTareaACompletado()
+        {
+            tarea.Iniciar();
+            tarea.Finalizar();
+
+            Assert.That(tarea.Estado, Is.EqualTo(EstadoTarea.Completado));
+            Assert.That(tarea.FechaFinalizacion, Is.Not.Null);
+        }
+
+        [Test]
+        public void AgregarSubtarea()
+        {
+            Tarea subtarea = new Tarea(
+                2,
+                "Subtarea",
+                "Descripcion de subtarea",
+                DateTime.Now.AddDays(1),
+                PrioridadTarea.Alta
+            );
+
+            tarea.AgregarSubtarea(subtarea);
+
+            Assert.That(tarea.Subtareas.Count, Is.EqualTo(1));
+        }
+
+        [Test]
+        public void GestorTareaPorId()
+        {
+            GestorTareas gestor = new GestorTareas();
+            gestor.AgregarTarea(tarea);
+
+            Tarea? resultado = gestor.BuscarPorId(1);
+
+            Assert.That(resultado, Is.Not.Null);
+            Assert.That(resultado.Id, Is.EqualTo(1));
+        }
     }
 }
